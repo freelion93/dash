4b4bb5aac8ec87edf2379d13bcffbd698df1ec41
freelion93 <gordon5193@mail.ru>
2016-06-08 13:35:12 +0300

    new
    new

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2010 Satoshi Nakamoto
 // Copyright (c) 2009-2014 The Bitcoin developers
-// Copyright (c) 2014-2015 The Mtucicoin developers
+// Copyright (c) 2014-2015 The Owncoin developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -56,11 +56,11 @@ static void convertSeed6(std::vector<CAddress> &vSeedsOut, const SeedSpec6 *data
 
 static Checkpoints::MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of
-        (  0, uint256("0x0000053fe8b5b3471c9b0fb626260190b842aac5e2679052d0dde666c971c372"))
+        (  0, uint256("0x00000205a2b36c89d27547db95cdf4a3038662dd26cf21af8b9c8978a64b06a2"))
         ;
 static const Checkpoints::CCheckpointData data = {
         &mapCheckpoints,
-        1465318800, // * UNIX timestamp of last checkpoint block
+        1449579900, // * UNIX timestamp of last checkpoint block
         0,     // * total number of transactions between genesis and last checkpoint
                     //   (the tx=... number in the SetBestChain debug.log lines)
         2800        // * estimated number of transactions per day after checkpoint
@@ -68,18 +68,18 @@ static const Checkpoints::CCheckpointData data = {
 
 static Checkpoints::MapCheckpoints mapCheckpointsTestnet =
         boost::assign::map_list_of
-        ( 0, uint256("00000850a016bc07212bc16cb69dcee1f323d7aeb804be66140f527c171a8623"))
+        ( 0, uint256("00000416024caa7a7900f9a73c120660773ca4019d0671ee5d5bb528acbaa633"))
         ;
 static const Checkpoints::CCheckpointData dataTestnet = {
         &mapCheckpointsTestnet,
-        1465318801,
+        1449579901,
         0,
         500
     };
 
 static Checkpoints::MapCheckpoints mapCheckpointsRegtest =
         boost::assign::map_list_of
-        ( 0, uint256("0079ee9ce1cc53cfe679961fc4ef7875c674b762d7f6f5987d7bdec6a487f491"))
+        ( 0, uint256("0x17a08f326f61be14e0502edeb93d46013596a0bf1d8ddd9d5909f37dab7305a4"))
         ;
 static const Checkpoints::CCheckpointData dataRegtest = {
         &mapCheckpointsRegtest,
@@ -102,16 +102,16 @@ public:
         pchMessageStart[1] = 0xbd;
         pchMessageStart[2] = 0xcb;
         pchMessageStart[3] = 0x4f;
-        vAlertPubKey = ParseHex("04820a5299695d90e1b228fbe03bc8471d216fc4563582b0daee3454c8878be16daa1b350163345ee144c25dd214207f439a36c2ec8eebbfc22abb0100b48cfbb0");
+        vAlertPubKey = ParseHex("04f4ef416b8597cbadde2216eb0d4fc5137075a3fa3442b71b4ea97efca14e885aa75911b7ef3e6353281d2515a375e8a510455214031df1ac46e443e1ea9a9647");
         nDefaultPort = 9887;
-        bnProofOfWorkLimit = ~uint256(0) >> 20;  // Mtucicoin starting difficulty is 1 / 2^12
+        bnProofOfWorkLimit = ~uint256(0) >> 20;  // Owncoin starting difficulty is 1 / 2^12
         nSubsidyHalvingInterval = 210000;
         nEnforceBlockUpgradeMajority = 750;
         nRejectBlockOutdatedMajority = 950;
         nToCheckBlockUpgradeMajority = 1000;
         nMinerThreads = 0;
-        nTargetTimespan = 1 * 60 * 60; // Mtucicoin: 1 hour
-        nTargetSpacing = 1.5 * 60; // Mtucicoin: 1.5 minutes
+        nTargetTimespan = 24 * 60 * 60; // Owncoin: 1 day
+        nTargetSpacing = 2.5 * 60; // Owncoin: 2.5 minutes
 
         /**
          * Build the genesis block. Note that the output of the genesis coinbase cannot
@@ -123,7 +123,7 @@ public:
          *     CTxOut(nValue=50.00000000, scriptPubKey=0xA9037BAC7050C479B121CF)
          *   vMerkleTree: e0028e
          */
-        const char* pszTimestamp = "I close to finish my diploma project 07/05/2016, I hope get good score, thanks";
+        const char* pszTimestamp = "I create my owncoin 07/12/2015 for test , End its work for me. I hope this guide help you";
         CMutableTransaction txNew;
         txNew.vin.resize(1);
         txNew.vout.resize(1);
@@ -134,22 +134,22 @@ public:
         genesis.hashPrevBlock = 0;
         genesis.hashMerkleRoot = genesis.BuildMerkleTree();
         genesis.nVersion = 1;
-        genesis.nTime    = 1465318800;
+        genesis.nTime    = 1449579900;
         genesis.nBits    = 0x1e0ffff0;
-        genesis.nNonce   = 107633;
+        genesis.nNonce   = 2087222;
 
         hashGenesisBlock = genesis.GetHash();
-        assert(hashGenesisBlock == uint256("0x0000053fe8b5b3471c9b0fb626260190b842aac5e2679052d0dde666c971c372"));
-        assert(genesis.hashMerkleRoot == uint256("0x93ba3974d0b5cda42a400e00f2e32f0ba958121575eab0e0aa7c13687fcb7467"));
+        assert(hashGenesisBlock == uint256("0x00000205a2b36c89d27547db95cdf4a3038662dd26cf21af8b9c8978a64b06a2"));
+        assert(genesis.hashMerkleRoot == uint256("0xb7ba7a0616705a327d39f6bf791c7db48abe4c79a11b62c6d690816eddc4d730"));
 
         vSeeds.clear();
 
-        base58Prefixes[PUBKEY_ADDRESS] = list_of( 76);                    // Mtucicoin addresses start with 'X'
-        base58Prefixes[SCRIPT_ADDRESS] = list_of( 16);                    // Mtucicoin script addresses start with '7'
-        base58Prefixes[SECRET_KEY] =     list_of(204);                    // Mtucicoin private keys start with '7' or 'X'
-        base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x02)(0xFE)(0x52)(0xF8); // Mtucicoin BIP32 pubkeys start with 'drkv'
-        base58Prefixes[EXT_SECRET_KEY] = list_of(0x02)(0xFE)(0x52)(0xCC); // Mtucicoin BIP32 prvkeys start with 'drkp'
-        base58Prefixes[EXT_COIN_TYPE]  = list_of(0x80000005);             // Mtucicoin BIP44 coin type is '5'
+        base58Prefixes[PUBKEY_ADDRESS] = list_of( 76);                    // Owncoin addresses start with 'X'
+        base58Prefixes[SCRIPT_ADDRESS] = list_of( 16);                    // Owncoin script addresses start with '7'
+        base58Prefixes[SECRET_KEY] =     list_of(204);                    // Owncoin private keys start with '7' or 'X'
+        base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x02)(0xFE)(0x52)(0xF8); // Owncoin BIP32 pubkeys start with 'drkv'
+        base58Prefixes[EXT_SECRET_KEY] = list_of(0x02)(0xFE)(0x52)(0xCC); // Owncoin BIP32 prvkeys start with 'drkp'
+        base58Prefixes[EXT_COIN_TYPE]  = list_of(0x80000005);             // Owncoin BIP44 coin type is '5'
 
         convertSeed6(vFixedSeeds, pnSeed6_main, ARRAYLEN(pnSeed6_main));
 
@@ -163,10 +163,10 @@ public:
         fTestnetToBeDeprecatedFieldRPC = false;
 
         nPoolMaxTransactions = 3;
-        strSporkKey = "047fc00c0bef93b2a205461487356b33742f778b2a9b5081eb95f628693fa5b799cb61f2b8a75eac29e18ac35109d15f6cf2d5a4dd740a86634d20a6fb799faa05";
-        strMasternodePaymentsPubKey = "047fc00c0bef93b2a205461487356b33742f778b2a9b5081eb95f628693fa5b799cb61f2b8a75eac29e18ac35109d15f6cf2d5a4dd740a86634d20a6fb799faa05";
+        strSporkKey = "04594d9bcfd432ed21603c81dc6a2cbdc58ded77217ecd91c0420737f60a1948871a66bb84bff26e0f5dd447ee931b5213bca0d9de686c49f6f03545a03fd79b75";
+        strMasternodePaymentsPubKey = "04594d9bcfd432ed21603c81dc6a2cbdc58ded77217ecd91c0420737f60a1948871a66bb84bff26e0f5dd447ee931b5213bca0d9de686c49f6f03545a03fd79b75";
         strDarksendPoolDummyAddress = "Xq19GqFvajRrEdDHYRKGYjTsQfpV5jyipF";
-        nStartMasternodePayments = 1465318800; //new date
+        nStartMasternodePayments = 1449579900; //Wed, 25 Jun 2014 20:36:16 GMT
     }
 
     const Checkpoints::CCheckpointData& Checkpoints() const 
@@ -188,35 +188,35 @@ public:
         pchMessageStart[1] = 0xbc;
         pchMessageStart[2] = 0xc7;
         pchMessageStart[3] = 0x4c;
-        vAlertPubKey = ParseHex("04820a5299695d90e1b228fbe03bc8471d216fc4563582b0daee3454c8878be16daa1b350163345ee144c25dd214207f439a36c2ec8eebbfc22abb0100b48cfbb0");
+        vAlertPubKey = ParseHex("04b0105bfdf3a50cc51a503caeb23526d62b7fcc3e5bd6f451eb29fd66846714ba3af73ecc317d9b25df525f03b76e22d14a8e7d8631fcc89cee9122a1cc6420c0");
         nDefaultPort = 19887;
         nEnforceBlockUpgradeMajority = 51;
         nRejectBlockOutdatedMajority = 75;
         nToCheckBlockUpgradeMajority = 100;
         nMinerThreads = 0;
-        nTargetTimespan = 1 * 60 * 60; // Mtucicoin: 1 hour
-        nTargetSpacing = 1.5 * 60; // Mtucicoin: 1.5 minutes
+        nTargetTimespan = 24 * 60 * 60; // Owncoin: 1 day
+        nTargetSpacing = 2.5 * 60; // Owncoin: 2.5 minutes
 
         //! Modify the testnet genesis block so the timestamp is valid for a later start.
-        genesis.nTime = 1465318801;
-        genesis.nNonce = 2387307;
+        genesis.nTime = 1449579901;
+        genesis.nNonce = 1899912;
 
         hashGenesisBlock = genesis.GetHash();
-        assert(hashGenesisBlock == uint256("0x00000850a016bc07212bc16cb69dcee1f323d7aeb804be66140f527c171a8623"));
+        assert(hashGenesisBlock == uint256("0x00000416024caa7a7900f9a73c120660773ca4019d0671ee5d5bb528acbaa633"));
 
         vFixedSeeds.clear();
         vSeeds.clear();
-        /*vSeeds.push_back(CDNSSeedData("freelion93.io", "testnet-seed.freelion93.io"));
-        vSeeds.push_back(CDNSSeedData("mtucicoin.qa", "testnet-seed.mtucicoin.qa"));
+        /*vSeeds.push_back(CDNSSeedData("mycointest.io", "testnet-seed.mycointest.io"));
+        vSeeds.push_back(CDNSSeedData("owncoin.qa", "testnet-seed.owncoin.qa"));
         *///legacy seeders
         vSeeds.clear();
 
-        base58Prefixes[PUBKEY_ADDRESS] = list_of(139);                    // Testnet mtucicoin addresses start with 'x' or 'y'
-        base58Prefixes[SCRIPT_ADDRESS] = list_of( 19);                    // Testnet mtucicoin script addresses start with '8' or '9'
+        base58Prefixes[PUBKEY_ADDRESS] = list_of(139);                    // Testnet owncoin addresses start with 'x' or 'y'
+        base58Prefixes[SCRIPT_ADDRESS] = list_of( 19);                    // Testnet owncoin script addresses start with '8' or '9'
         base58Prefixes[SECRET_KEY]     = list_of(239);                    // Testnet private keys start with '9' or 'c' (Bitcoin defaults)
-        base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x3a)(0x80)(0x61)(0xa0); // Testnet mtucicoin BIP32 pubkeys start with 'DRKV'
-        base58Prefixes[EXT_SECRET_KEY] = list_of(0x3a)(0x80)(0x58)(0x37); // Testnet mtucicoin BIP32 prvkeys start with 'DRKP'
-        base58Prefixes[EXT_COIN_TYPE]  = list_of(0x80000001);             // Testnet mtucicoin BIP44 coin type is '5' (All coin's testnet default)
+        base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x3a)(0x80)(0x61)(0xa0); // Testnet owncoin BIP32 pubkeys start with 'DRKV'
+        base58Prefixes[EXT_SECRET_KEY] = list_of(0x3a)(0x80)(0x58)(0x37); // Testnet owncoin BIP32 prvkeys start with 'DRKP'
+        base58Prefixes[EXT_COIN_TYPE]  = list_of(0x80000001);             // Testnet owncoin BIP44 coin type is '5' (All coin's testnet default)
 
         convertSeed6(vFixedSeeds, pnSeed6_test, ARRAYLEN(pnSeed6_test));
 
@@ -229,10 +229,10 @@ public:
         fTestnetToBeDeprecatedFieldRPC = true;
 
         nPoolMaxTransactions = 2;
-        strSporkKey = "047fc00c0bef93b2a205461487356b33742f778b2a9b5081eb95f628693fa5b799cb61f2b8a75eac29e18ac35109d15f6cf2d5a4dd740a86634d20a6fb799faa05";
-        strMasternodePaymentsPubKey = "047fc00c0bef93b2a205461487356b33742f778b2a9b5081eb95f628693fa5b799cb61f2b8a75eac29e18ac35109d15f6cf2d5a4dd740a86634d20a6fb799faa05";
+        strSporkKey = "041edf37d10503df7213bef9c07a13c1c94afe70ed8a4c85a79a658097bb36766e0330736acf2e921a3b97e507b270d779b454cbd4e7adf7032d9b02a37d5d680a";
+        strMasternodePaymentsPubKey = "041edf37d10503df7213bef9c07a13c1c94afe70ed8a4c85a79a658097bb36766e0330736acf2e921a3b97e507b270d779b454cbd4e7adf7032d9b02a37d5d680a";
         strDarksendPoolDummyAddress = "y1EZuxhhNMAUofTBEeLqGE1bJrpC2TWRNp";
-        nStartMasternodePayments = 1465318801; //new date
+        nStartMasternodePayments = 1449579901; //Fri, 09 Jan 2015 21:05:58 GMT
     }
     const Checkpoints::CCheckpointData& Checkpoints() const 
     {
@@ -258,15 +258,15 @@ public:
         nRejectBlockOutdatedMajority = 950;
         nToCheckBlockUpgradeMajority = 1000;
         nMinerThreads = 1;
-        nTargetTimespan = 1 * 60 * 60; // Mtucicoin: 1 day
-        nTargetSpacing = 1.5 * 60; // Mtucicoin: 2.5 minutes
+        nTargetTimespan = 24 * 60 * 60; // Owncoin: 1 day
+        nTargetSpacing = 2.5 * 60; // Owncoin: 2.5 minutes
         bnProofOfWorkLimit = ~uint256(0) >> 1;
-        genesis.nTime = 1465318802;
+        genesis.nTime = 1449579902;
         genesis.nBits = 0x207fffff;
-        genesis.nNonce = 0;
+        genesis.nNonce = 2;
         hashGenesisBlock = genesis.GetHash();
         nDefaultPort = 19883;
-        assert(hashGenesisBlock == uint256("0x0079ee9ce1cc53cfe679961fc4ef7875c674b762d7f6f5987d7bdec6a487f491"));
+        assert(hashGenesisBlock == uint256("0x17a08f326f61be14e0502edeb93d46013596a0bf1d8ddd9d5909f37dab7305a4"));
 
         vFixedSeeds.clear(); //! Regtest mode doesn't have any fixed seeds.
         vSeeds.clear();  //! Regtest mode doesn't have any DNS seeds.
